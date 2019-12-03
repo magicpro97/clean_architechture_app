65d51ee6f6304db11365572501776bdd181b4efe
linhngo <thlinh.ngo@gmail.com>
2019-12-03 15:21:00 +0700

    number trivia state

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,4 +1,6 @@
+import 'package:clean_architechture_app/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
 
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
@@ -7,4 +9,32 @@ abstract class NumberTriviaState extends Equatable {
 class InitialNumberTriviaState extends NumberTriviaState {
   @override
   List<Object> get props => [];
+}
+
+class Empty extends NumberTriviaState {
+  @override
+  List<Object> get props => null;
+}
+
+class Loading extends NumberTriviaState {
+  @override
+  List<Object> get props => null;
+}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia});
+
+  @override
+  List<Object> get props => [trivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message});
+
+  @override
+  List<Object> get props => [message];
 }
